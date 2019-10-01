758c13b285e77cdbe3c39b38a17059875e7984be
Tiago <tiago.spana@gmail.com>
2019-10-01 01:56:39 -0300

    Onlynumber from string

---
+++ strings/ExtractNumberFromText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace System
+{
+    public static class ExtractNumberFromText
+    {
+        public static string OnlyNumber(this string str)
+        {
+            return Regex.Replace(str, "[^0-9]", "");
+        }
+        public static string OnlyNumber(this string str, string ifEmpty)
+        {
+            var value = str.OnlyNumber();
+            if (string.IsNullOrEmpty(value))
+                return ifEmpty;
+            else
+                return value;
+        }
+    }
+}
