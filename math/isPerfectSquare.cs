3642185d2791791e7e3e135fe6c97843e3eff149
Ayoola Akindolani <aakindolani@eminenttechnology.com>
2019-10-08 00:24:36 +0100

    added c# to check if a number is a perfect square

---
+++ math/isPerfectSquare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// This is a c# algorithm to check 
+/// if a number is a perfect square
+/// </summary>
+public class Math
+{
+	public IsPerfectSquare(double num)
+	{
+        double result = Math.Sqrt(num);
+        bool isPerfectSquare = result % 1 == 0;
+	}
+
+
+    ///[Test]
+    ///isPerfectSquare returns true for all squares
+    ///isPerfectSquare returns false for non perfect squares
+}
