1de580842a7a321e33eba3c8fa3430ffe5d14693
JonathanDC64 <nintendo_jon64@hotmail.com>
2019-10-10 15:37:53 -0400

    Created string reversal algorithm

---
+++ strings/StringReverse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+namespace Algorithms
+{
+    public class StringReverse
+    {
+        public static string Reverse(string text)
+        {
+            int textEnd = text.Length - 1;
+            int iterateEnd = text.Length / 2;
+            StringBuilder textCopy = new StringBuilder(text);
+            for(int i = 0; i < iterateEnd; ++i)
+            {
+                char front = textCopy[i];
+                char back = textCopy[textEnd - i];
+                textCopy[i] = back;
+                textCopy[textEnd - i] = front;
+            }
+            return textCopy.ToString();
+        }
+    }
+}
